c41d2bb65d52251e37f925288d8d9163df507c81
TheFear36 <gpodvorotov@gmail.com>
2022-03-26 11:38:21 +0300

    Начал работу с бихевероами (Не успеваю доделать в срок)

---
+++ app/src/main/java/com/thefear/nasadesign/utils/ButtonBehavior.kt
@@ -0,0 +1,35 @@
+package com.thefear.nasadesign.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.appbar.AppBarLayout
+import hide
+import kotlin.math.abs
+
+class ButtonBehavior (context: Context, attr: AttributeSet) : CoordinatorLayout.Behavior<View>(context, attr) {
+
+    override fun layoutDependsOn(
+        parent: CoordinatorLayout,
+        child: View,
+        dependency: View
+    ) = dependency is AppBarLayout
+
+
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        button : View,
+        dependency: View
+    ): Boolean {
+        val bar = dependency as AppBarLayout
+        val barHeight = bar.height.toFloat()
+        val barY = bar.y
+
+        if (abs(barY)>(barHeight*0.6)) {
+            button.hide()
+        }
+
+        return super.onDependentViewChanged(parent, button, dependency)
+    }
+}
